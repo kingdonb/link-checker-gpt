219b177a9e84116efb920648fecc0ea2112a84b6
Kingdon Barrett <kingdon@weave.works>
2023-08-17 19:34:10 -0400

    fixup Dockerfile and entrypoint.sh
    Signed-off-by: Kingdon Barrett <kingdon@weave.works>

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -1,17 +1,17 @@
 #!/bin/sh -l
 
 # Run main target
-bundle exec make main
+bundle exec make -C /opt/link-checker main
 
 # Clean cache
-make clean-cache
+make -C /opt/link-checker clean-cache
 
 # Use the provided PR number to construct the preview URL
 PREVIEW_URL="deploy-preview-$INPUT_PRNUMBER--fluxcd.netlify.app"
 export PREVIEW_URL
 
 # Run with preview
-bundle exec make run_with_preview
+bundle exec make -C /opt/link-checker run_with_preview
 
 # Normalize reports
 make normalize
@@ -20,7 +20,7 @@ make normalize
 bundle exec make summary
 
 # Check summary results
-./scripts/check_summary.sh
+/opt/link-checker/scripts/check_summary.sh
 
 # TODO: the workflow should nag issues in the baseline
-# ./scripts/comment_on_pr.sh
+# /opt/link-checker/scripts/comment_on_pr.sh
