e8b8708370a55dde0609c11f50cefa4d0e09a0b1
Kingdon Barrett <kingdon@weave.works>
2023-08-21 13:23:01 -0400

    there is no env
    the advice to use env is for scripts that users embed in their own repos
    https://docs.github.com/en/actions/using-workflows/using-github-cli-in-workflows
    
    we need to take githubToken as an input, evidently
    
    Signed-off-by: Kingdon Barrett <kingdon@weave.works>

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,6 +14,8 @@ export PREVIEW_URL="$INPUT_PREVIEWDOMAIN"
 export PR_NUMBER="$INPUT_PRNUMBER"
 
 timeout 60s bash -c 'while curl -sSL "https://${PREVIEW_URL}/sitemap.xml" |grep "Not Found - Request ID: "; do sleep 5; done'
+
+export GH_TOKEN="$INPUT_GITHUBTOKEN"
 timeout 60s bash -c 'until gh pr checks ${PR_NUMBER}|grep netlify/fluxcd/deploy-preview|grep "Deploy Preview ready!"; do sleep 5; done'
 
 # Run with preview
